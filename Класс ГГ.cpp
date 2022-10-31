5457b4abf578e5f7fd57536c74df58f86c3aefc9
Tihendii <47138445+Tihendii@users.noreply.github.com>
2022-10-31 12:48:52 +0300

    Add files via upload

---
+++ Класс ГГ.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class gg {
+    public:
+        int changeHp(int value){
+            ggHp = value;
+        }
+        int getHp(){
+            return ggHp;
+        }
+    private:
+        int ggHp;
+        bool isDry;
+        bool isFlaming;
+        
+};
+int main()
+{
+    
+};
