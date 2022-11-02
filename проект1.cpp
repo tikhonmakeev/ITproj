1c1d59d267c949ce4c181f862559e9f1cfde0e79
Sergey Panteleev <mrcatmen.1@gmail.com>
2022-11-02 16:43:21 +0300

    yg

--- проект1.cpp
+++ проект1.cpp
@@ -2,13 +2,15 @@
 #include <string>
 using namespace std;
 string magick(element_1,element_2){
+    
+     
     if (element_1 = "water" and element_2 = "fire"){
         return "steam";
     }
     if (element_1 = "water" and element_2 = "cold"){
         return "ice";
     }
-    if (element_1 = "water" and element_2 = "earth"){
+    if (element_1 = "water" and element_2 = "earth"){ 
         return "dirt";
     }
     if (element_1 = "water" and element_2 = "fire"){
@@ -17,7 +19,7 @@ string magick(element_1,element_2){
 }
 int main() {
 
-    
+
     string element_1 = "water";
     string element_2 = "fire";
     magick(element_1, element_2);
