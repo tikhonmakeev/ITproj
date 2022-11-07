412174235e6b518f6ea06b085ac2497a9a23367d
Tihendii <47138445+Tihendii@users.noreply.github.com>
2022-11-07 11:56:30 +0300

    Update Боевка с Орком.cpp
    Добавлен блок удара(у гг)

--- Боевка с Орком.cpp
+++ Боевка с Орком.cpp
@@ -13,7 +13,7 @@ class gg {
         int hp;
         bool isDry;
         bool isFlaming;
-        
+        bool isBloking;
 };
 class orc{
     public:
@@ -35,12 +35,17 @@ int main()
     Smorch.setHp(500);
     while(Kolyan.getHp()>0 & Smorch.getHp()>0){
         int damage = rand()%20;
-        Kolyan.setHp(Kolyan.getHp()-damage);
-        if (Kolyan.getHp()<0){
-        	cout<<"Вам нанесли добивающий удар("<<damage<<" урона)."<<endl;
-        	break;
+        if (isBlocking){
+            cout<<"Вы заблокировали удар("<<damage<<" урона)."<<endl;
         }
-        cout<<"Вам нанесли "<<damage<<" урона! Ваш уровень здоровья: "<<Kolyan.getHp()<<endl;
+        else{
+            Kolyan.setHp(Kolyan.getHp()-damage);
+            if (Kolyan.getHp()<0){
+                cout<<"Вам нанесли добивающий удар("<<damage<<" урона)."<<endl;
+                break;
+            }
+            cout<<"Вам нанесли "<<damage<<" урона! Ваш уровень здоровья: "<<Kolyan.getHp()<<endl;
+        }
         cout<<"Куда нанести удар?"<<endl<<"Голова/Туловище/Ноги"<<endl;
         string wherePunched;
         cin>>wherePunched;
